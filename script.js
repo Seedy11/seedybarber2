5be5bd8db28a6273564f6358928bffbda6ff2ca4
Seedy11 <64992816+Seedy11@users.noreply.github.com>
2021-05-07 02:51:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// 	maxZoom: 30,
+// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
+// L.marker([51.5, -0.09]).addTo(mymap);
+
+// //mIL2hwXdfUJSM07s9TGDmBFZNppCNw-DPIiIGB33HVc
